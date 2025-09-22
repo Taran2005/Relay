471485c60d8d0c50203a92808bf8239fd0451f16
Taran2005 <tarangadh@gmail.com>
2025-09-22 23:27:23 +0530

    refactor: standardize indentation and formatting across server components for improved readability

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -2,18 +2,18 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 interface UserAvatarProps {
-  src?: string | null;
-  className?: string;
+    src?: string | null;
+    className?: string;
 }
 
 export const UserAvatar = ({ src, className }: UserAvatarProps) => {
-  return (
-    <Image
-      src={src ?? "/default-avatar.png"}
-      alt="User Avatar"
-      width={32}
-      height={32}
-      className={cn("h-8 w-8 rounded-full", className)}
-    />
-  );
+    return (
+        <Image
+            src={src ?? "/default-avatar.png"}
+            alt="User Avatar"
+            width={32}
+            height={32}
+            className={cn("h-8 w-8 rounded-full", className)}
+        />
+    );
 };
